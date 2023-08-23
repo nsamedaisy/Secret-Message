2700b6eddb7ff39b99d66450719e5f812eab6f69
nsamedaisy <nsamedaisy@gmail.com>
2023-08-23 11:01:27 +0100

    secret message commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const input = document.getElementById("input");
+const encryptBtn = document.getElementById("encrypt-btn");
+const rectangle = document.getElementById("rectangle");
+const encoded = document.getElementById("encoded");
+const decoded = document.getElementById("decoded");
+
+
