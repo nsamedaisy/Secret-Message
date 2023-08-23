de1bdcbfc62caaea67cac0d404a83305e6c6c650
nsamedaisy <nsamedaisy@gmail.com>
2023-08-23 11:33:42 +0100

    secret message commit

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ const rectangle = document.getElementById("rectangle");
 const encoded = document.getElementById("encoded");
 const decoded = document.getElementById("decoded");
 
-
+//Function to normalize the input text
+function normalizeText(text) {
+  return text.toLowerCase().replace(/[^a-z]/g, "");
+}
